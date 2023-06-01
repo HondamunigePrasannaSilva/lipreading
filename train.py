b370dae07b7bfcff4a9571ffc99e6f8e1ddc13c7
HondamunigePrasannaSilva <praslv65@gmail.com>
2023-06-01 11:43:27 +0200

    refactor train

--- train.py
+++ train.py
@@ -1,10 +1,84 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+#from torchtext.legacy.datasets import Multi30k
+#from torchtext.legacy.data import Field, BucketIterator
+
+#import spacy
+import numpy as np
+
+import random
+import math
+import time
+
+# model file, encoder, decoder and seqtoseq
+from model import *
+# utils file
+from utils import *
+# Get landmark using vocadataset.py
+from data.vocaset import *
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+
+trainset = vocadataset("train", landmark=True)
+dataloader = DataLoader(trainset, batch_size=32, collate_fn=collate_fn)
+
+
+vocabulary = create_vocabulary(blank='@')
+
+LANDMARK_DIM = 68
+INPUT_DIM = LANDMARK_DIM*3
+HID_DIM = 128
+output_dim = len(vocabulary)
+
+enc = Encoder(INPUT_DIM, HID_DIM)
+dec = Decoder(output_dim, HID_DIM)
+model = Seq2Seq(enc, dec, 'cpu')
 
 
 
 
 
-"""
 
-    TRAINING PIPELINE
 
-"""
+
+
+# With batch
+
+# Define the CTC loss function
+ctc_loss = nn.CTCLoss()
+
+# Define the optimizer
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training loop
+num_epochs = 100
+for epoch in range(num_epochs):
+    for landmarks, len_landmark, label, len_label in dataloader:
+        # reshape the batch from [batch_size, frame_size, num_landmark, 3] to [batch_size, frame_size, num_landmark * 3] 
+        landmarks = torch.reshape(landmarks, (landmarks.shape[0], landmarks.shape[1], landmarks.shape[2]*landmarks.shape[3]))
+        # label to char
+        optimizer.zero_grad()
+        
+        label = char_to_index_batch(label, vocabulary)
+
+        output = model(landmarks, label)
+        
+
+        input_lengths = torch.full((1,), output.size(0), dtype=torch.long)
+
+        #target_lengths = torch.full((target_tensor.size(0),), target_tensor.size(0), dtype=torch.long)
+        
+        loss = ctc_loss(output, label, input_lengths, len_label)
+        loss.backward()
+        optimizer.step()
+
+        if (epoch + 1) % 10 == 0:
+            print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}")
+            #e = torch.argmax(output, dim=2).squeeze(1)
+            #output_sequence = ''.join([vocabulary[index] for index in e])
+            #print(output_sequence)
+            torch.save(model.state_dict(), "models/model.pt")

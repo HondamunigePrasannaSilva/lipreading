0323a261974bcee288a7f145cbb6b76c51d9b07f
GiovanniMaccioni <maccionig.801@gmail.com>
2023-07-08 08:56:48 +0200

    fix

--- seq2seq.py
+++ seq2seq.py
@@ -1,19 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
-#from torchtext.legacy.datasets import Multi30k
-#from torchtext.legacy.data import Field, BucketIterator
-
-#import spacy
 import numpy as np
 
 import random
 import math
 import time
-
-
-
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
